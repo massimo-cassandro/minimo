a9969cccda61a14e1558cefe77f0684a6ce68a37
Massimo Cassandro <massimo.cassandro@gmail.com>
2026-01-29 16:54:03 +0100

    upd

--- package/src/js/dismiss-alerts.js
+++
@@ -1,6 +0,0 @@
-// dismiss alerts
-document.querySelectorAll('[data-dismiss]').forEach(item => {
-  item.addEventListener('click', () => {
-    item.closest('.' + item.dataset.dismiss).remove();
-  }, false);
-});
