7846a2591ea10df5ec1f923e196f789ba905ace9
Massimo Cassandro <massimo.cassandro@gmail.com>
2026-06-24 11:59:53 +0200

    fix classnames: check for non string values

--- src/utilities/classnames.js
+++ src/utilities/classnames.js
@@ -1,3 +1,10 @@
 export function classnames(...args) {
-  return args.filter(Boolean).join(' ');
+  return args.flat().filter(x => {
+    if (x && typeof x !== 'string') {
+      // eslint-disable-next-line no-console
+      console.error('[classnames] valore non stringa ignorato in ', ...args);
+      return false;
+    }
+    return Boolean(x);
+  }).join(' ');
 }
